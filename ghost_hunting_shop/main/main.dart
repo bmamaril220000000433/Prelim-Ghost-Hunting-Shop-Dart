5b9309314bd96da50be77a833e86e060b6e233fc
bmamaril220000000433 <142190594+bmamaril220000000433@users.noreply.github.com>
2024-09-21 15:53:57 +0800

    Update main.dart

--- ghost_hunting_shop/main/main.dart
+++ ghost_hunting_shop/main/main.dart
@@ -1,20 +1,32 @@
 import 'dart:io';
 
+import 'package:riverpod/riverpod.dart';
+import 'global_providers.dart';
+
 void main(){
+  final container = ProviderContainer();
+  final allitems = container.read(itemsProvider);
+  final shop = container.read(shopProvider);
+  
+
   int checker = 1;
   
   while (checker == 1){
-    print("-->Welcome to Ghost Hunters Shop<-- \n[1] Buy an Equipment \n[2] Sell an Equipment \n[3] View all your equipments \n[0] End");
+    print("-->Welcome to Ghost Hunters Shop<-- \n"
+    "[1] Buy an Equipment \n"
+    "[2] Sell an Equipment \n"
+    "[3] View all your equipments \n"
+    "[0] End");
     String? choose = stdin.readLineSync();
     if(choose == "1"){
     print("Buying which equipment?");
-
+      shop.purchase();
     }else if(choose == "2"){
       print("Selling which equipment?");
-      break;
+      break; // Temporary break
     }else if(choose == "3"){
       print("Equipments Owned:");
-      break;
+      allitems.getAllItems();
     }else if(choose == "0"){
       print("you exited the shop, goodbye!");
       checker = 0;
@@ -23,5 +35,4 @@ void main(){
       checker = 0;
     }
   }
-  
 }
