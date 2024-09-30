c818ea8ab351a385633d47000129f7f356ca61c5
bmamaril220000000433 <142190594+bmamaril220000000433@users.noreply.github.com>
2024-09-30 08:09:02 +0800

    Delete ghost_hunting_shop/main/global_providers.dart

--- ghost_hunting_shop/main/global_providers.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ghost_hunting_shop/services/inventory.dart';
-import 'package:ghost_hunting_shop/services/purchaseitem.dart';
-import 'package:ghost_hunting_shop/services/viewallitems.dart';
-import 'package:riverpod/riverpod.dart';
-
-final inventoryProvider = Provider<Inventory>((ref) => Inventory());
-final shopProvider = Provider((ref) => Shop(ref));
-final itemsProvider = Provider((ref) => Allitems(ref));
